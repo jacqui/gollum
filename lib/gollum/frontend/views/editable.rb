c6396f9410b3cb376d4bf78eee9943289c4c0099
Jacqueline Cox <jacqui@brighter.net>
2011-11-22 13:02:01 -0500

    limit options to just markdown

--- lib/gollum/frontend/views/editable.rb
+++ lib/gollum/frontend/views/editable.rb
@@ -1,13 +1,7 @@
 module Precious
   module Editable
     def formats(selected = @page.format)
-      Gollum::Page::FORMAT_NAMES.map do |key, val|
-        { :name     => val,
-          :id       => key.to_s,
-          :selected => selected == key}
-      end.sort do |a, b|
-        a[:name].downcase <=> b[:name].downcase
-      end
+      [ { :name => "Markdown", :id => "markdown", :selected => true } ]
     end
   end
 end
